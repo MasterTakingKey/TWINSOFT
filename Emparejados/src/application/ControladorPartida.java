06afd39b00e534c09a46687926f378152a85c5da
Carlos Martinez Jimenez <cmartinezzzz19@gmail.com>
2020-04-14 14:24:34 +0200

    Creada la intefaz de partida y el controlador

---
+++ Emparejados/src/application/ControladorPartida.java
@@ -0,0 +1,78 @@
+package application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class ControladorPartida {
+
+    @FXML
+    private Label tiempo;
+
+    @FXML
+    private Label puntos;
+
+    @FXML
+    private Button pausa;
+
+    @FXML
+    private ImageView usuario;
+
+    @FXML
+    private ImageView carta_0_0;
+
+    @FXML
+    private ImageView carta_1_0;
+
+    @FXML
+    private ImageView carta_2_0;
+
+    @FXML
+    private ImageView carta_0_1;
+
+    @FXML
+    private ImageView carta_3_0;
+
+    @FXML
+    private ImageView carta_1_1;
+
+    @FXML
+    private ImageView carta_2_1;
+
+    @FXML
+    private ImageView carta_3_1;
+
+    @FXML
+    private ImageView carta_0_2;
+
+    @FXML
+    private ImageView carta_1_2;
+
+    @FXML
+    private ImageView carta_2_2;
+
+    @FXML
+    private ImageView carta_3_2;
+
+    @FXML
+    private ImageView carta_1_3;
+
+    @FXML
+    private ImageView carta_2_3;
+
+    @FXML
+    private ImageView carta_3_3;
+
+    @FXML
+    void muestracarta(MouseEvent event) {
+
+    }
+
+    @FXML
+    void pausar_partida(ActionEvent event) {
+
+    }
+
+}

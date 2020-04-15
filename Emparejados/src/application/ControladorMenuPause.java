f458c9e2916ab55607731236faac08e215a5b7a6
marcgarcia <marcgarcia@localhost>
2020-04-15 17:47:21 +0200

    Interfaz del menú de pausa e imágenes añadidas

---
+++ Emparejados/src/application/ControladorMenuPause.java
@@ -0,0 +1,41 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class ControladorMenuPause {
+
+    @FXML
+    private ImageView imagePlay;
+
+    @FXML
+    private ImageView imageRetry;
+
+    @FXML
+    private ImageView imageClose;
+
+    @FXML
+    private ImageView imageSound;
+
+    @FXML
+    void clickClose(MouseEvent event) {
+
+    }
+
+    @FXML
+    void clickPlay(MouseEvent event) {
+
+    }
+
+    @FXML
+    void clickRetry(MouseEvent event) {
+
+    }
+
+    @FXML
+    void clickSound(MouseEvent event) {
+
+    }
+
+}
